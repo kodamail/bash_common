cdaee9efa8fa04a2271332f8be1dd5312e4f10c5
Chihiro Kodama <kodamac@jamstec.go.jp>
2021-01-06 15:09:29 +0900

    add error_exit

--- bash_common.sh
+++ bash_common.sh
@@ -10,17 +10,16 @@
 #     temporary directory
 #
 export LANG=C
+export LC_ALL=C
 export F_UFMTENDIAN="big"
 
 BASH_COMMON_TEMP_DIR="none"
 BASH_COMMON_ORG_DIR=$( pwd )
 function create_temp()
 {
-    local TEMP
     for(( i=1; $i<=10; i=$i+1 )) ; do
-        TEMP=$( date +%s )
-    BASH_COMMON_TEMP_DIR=temp.$( date +%s ).$$
-        [ ! -d ${BASH_COMMON_TEMP_DIR} ] && break
+	BASH_COMMON_TEMP_DIR=temp.$( hostname -s ).$( date +%s ).$$
+        [[ ! -d ${BASH_COMMON_TEMP_DIR} ]] && break
         sleep 1s
     done
     mkdir -p ${BASH_COMMON_TEMP_DIR}
@@ -36,19 +35,27 @@ function finish()
     local MESSAGE=$1
 
     cd ${BASH_COMMON_ORG_DIR}
-    [ "${BASH_COMMON_TEMP_DIR}" != "" ] && rm -rf ${BASH_COMMON_TEMP_DIR}
+    [[ "${BASH_COMMON_TEMP_DIR}" != "" ]] && rm -rf ${BASH_COMMON_TEMP_DIR}
 
-#    echo ""
-#    if [ "${MESSAGE}" = "" ] ; then
-#	echo "########## $0 finished ##########"
-#	echo ""
-#    else
-    if [ "${MESSAGE}" != "" ] ; then
+    if [[ "${MESSAGE}" != "" ]] ; then
 	echo "${MESSAGE}"
 	echo ""
     fi
 }
 
+
+#
+# error handling (do not use from common.sh)
+#
+function error_exit()
+{
+    local msg=${1:-}
+    local ret=${2:-1}
+    echo "Error in $0 (line: ${BASH_LINENO[0]}): ${msg}" >&2
+    exit ${ret}
+}
+
+# legacy
 function exit_error()
 {
     local MESSAGE=$1
@@ -68,12 +75,12 @@ function exit_error()
 #
 function file_exist_or_error()
 {
-    if [ "$1" = "" ] ; then
+    if [[ "$1" = "" ]] ; then
 	exit_error "error in $0: filename is not specified for exist_file_or_error()."
     fi
 
-    while [ "$1" != "" ] ; do
-	if [ ! -f "$1" ] ; then
+    while [[ "$1" != "" ]] ; do
+	if [[ ! -f "$1" ]] ; then
 	    exit_error "error in $0: $1 does not exist."
 	fi
 	shift
@@ -82,10 +89,9 @@ function file_exist_or_error()
 
 function get_filesize()
 {
-    if [ "$1" = "" ] ; then
+    if [[ "$1" = "" ]] ; then
 	exit_error "error in $0: filename is not specified for get_filesize()."
     fi
-    SIZE=$( ls -lL $1 | awk '{ print $5 }' ) \
-	|| exit_error
+    SIZE=$( ls -lL $1 | awk '{ print $5 }' ) || exit_error
     echo ${SIZE}
 }
